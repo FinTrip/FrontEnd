d9b8895eb298a5166588eaa4ff2158c696178dec
bevisnguyen1301 <thanhbangbang95@gmail.com>
2025-04-14 14:51:01 +0700

    video call

--- src/app/callvideo/page.tsx
+++ src/app/callvideo/page.tsx
@@ -1,8 +1,20 @@
 // FrontEnd/src/app/CallVideo/page.tsx
 "use client";
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import VideoCall from '../page/components/callvideo/videocall';
 
-
 export default function VideoCallPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Kiểm tra xác thực từ localStorage
+    const localToken = localStorage.getItem('token');
+    
+    if (!localToken) {
+      router.push('/page/auth/login');
+    }
+  }, [router]);
+
   return <VideoCall />;
 }
